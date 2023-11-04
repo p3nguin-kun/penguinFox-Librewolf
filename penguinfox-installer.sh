b90def09438318fdbd7d51ddc1e36f91c95a5c42
p3nguin-kun <p3nguinkun@proton.me>
2023-11-04 19:55:55 +0700

    update

--- penguinfox-installer.sh
+++ penguinfox-installer.sh
@@ -16,8 +16,6 @@ date=$(date +%Y%m%d-%H%M%S)
 logo() {
 
 	local text="${1:?}"
-	echo -en "                                 
-  	penguinFox Installer (Librewolf ver.)\n\n"
 	printf ' %s [%s%s %s%s %s]%s\n\n' "${CRE}" "${CNC}" "${CYE}" "${text}"
 }
 
@@ -25,16 +23,6 @@ logo() {
 clear
 logo "Welcome!"
 printf '%s%sThis script will automatically install penguinFox - my Firefox config to your system %s\n\n' "${BLD}" "${CRE}" "${CNC}"
-
-while true; do
-	read -rp " Do you want to continue? [y/n]: " yn
-	case $yn in
-	[Yy]*) break ;;
-	[Nn]*) exit ;;
-	*) printf "Just write 'y' or 'n'\n\n" ;;
-	esac
-done
-clear
 
 # Backup config files
 logo "Backing-up your config files"
@@ -45,40 +33,23 @@ if [ ! -d "$backup_folder" ]; then
 fi
 
 for folder in chrome; do
-	if [ -d "$HOME"/.librewolf/*.default-default/$folder ]; then
-		mv "$HOME"/.librewolf/*.default-default/$folder "$backup_folder"/${folder}_$date
-		echo "$folder folder backed up successfully at $backup_folder/${folder}_$date"
-	else
-		echo "The folder $folder does not exist in $HOME/.librewolf/"
+	if [ -d "$HOME"/.librewolf/*.default-*/$folder ]; then
+		mv "$HOME"/.librewolf/*.default-*/$folder "$backup_folder"/${folder}_$date
 	fi
 done
 
 for file in user.js; do
-	if [ -e "$HOME"/.librewolf/*.default-default/$file ]; then
-		mv "$HOME"/.librewolf/*.default-default/$file "$backup_folder"/${file}_$date
-		echo "$file file backed up successfully at $backup_folder/${file}_$date"
-	else
-		echo "The file $file does not exist in $HOME/.librewolf/"
+	if [ -e "$HOME"/.librewolf/*.default-*/$file ]; then
+		mv "$HOME"/.librewolf/*.default-*/$file "$backup_folder"/${file}_$date
 	fi
 done
 
 # Installing dotfiles
 logo "Installing penguinFox..."
-printf "Copying files to respective directories..\n"
+cp -R ./* ~/.librewolf/*.default-release/
 
-for archivos in ./*; do
-	cp -R "${archivos}" ~/.librewolf/*.default-default/
-	if [ $? -eq 0 ]; then
-		printf "%s%s%s folder copied succesfully!%s\n" "${BLD}" "${CGR}" "${archivos}" "${CNC}"
-	else
-		printf "%s%s%s failed to been copied, you must copy it manually%s\n" "${BLD}" "${CRE}" "${archivos}" "${CNC}"
-		sleep 1
-	fi
-done
-
-# Removing unused files
 logo "Removing unused files..."
-rm -rf ~/.librewolf/*.default-default/.git ~/.librewolf/*.default-default/.github ~/.librewolf/*.default-default/penguinfox-installer.sh ~/.librewolf/*.default-default/README.md
+rm -rf ~/.librewolf/*.default-*/.git ~/.librewolf/*.default-*/.github ~/.librewolf/*.default-*/penguinfox-installer.sh ~/.librewolf/*.default-*/README.md
 
 logo "Done!"
-printf "Completed penguinFox installation, now open Librewolf and enjoy!"
+printf "Completed penguinFox installation, now open Firefox and enjoy!\n"
